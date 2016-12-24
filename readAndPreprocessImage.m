eb556d5c54507aa9edb50b5d2dccd6088e5b8b5d
Aaron Reich <agr505@gmail.com>
2016-12-23 22:29:17 -0500

    File 4

---
+++ readAndPreprocessImage.m
@@ -0,0 +1,17 @@
+% Read and pre-process images
+% Copyright 2016 The MathWorks, Inc.
+function Iout = readAndPreprocessImage(filename)
+
+I = imread(filename);
+
+% Some images may be grayscale. Replicate the image 3 times to
+% create an RGB image.
+if ismatrix(I)
+    I = cat(3,I,I,I);
+end
+
+% Resize the image as required for the CNN.
+Iout = imresize(I, [227 227]);
+Iout = imgaussfilt(Iout, 1);
+
+end
